32c0d2944dc2bcf80a8a7db8bef5b137a3fd229c
DongMin-0604 <dongmin3147@gmail.com>
2021-08-26 23:10:27 +0900

    사용자 화면 세로로 고정하는 코드 추가,앱 아이콘 변경 및 스플래시 화면 추가,앱 이름 변경,이슈 없음

---
+++ app/src/main/java/com/example/mento_menti_app/IntroActivity.java
@@ -0,0 +1,32 @@
+package com.example.mento_menti_app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IntroActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.intro_activity);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        },1500);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
